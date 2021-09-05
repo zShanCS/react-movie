570ca0a6937b8bce9b60794bd35363ad26e2241e
zShan Ahmad <mahmad.zeeshan72@gmail.com>
2021-09-05 10:42:21 +0500

    actor component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { } from "react";
 import { useParams } from "react-router-dom";
-import API from "../API";
 import useMovieFetch from "../hooks/useMovieFetch";
 import Spinner from './Spinner/index'
 import BreadCrumb from './BreadCrumb';
 import MovieInfo from "./MovieInfo";
 import MovieInfoBar from "./MovieInfoBar";
+import Actor from "./Actor";
+import Grid from './Grid'
+import NoImage from '../images/no_image.jpg'
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
 const Movie = () => {
 
   const { movieId } = useParams();
@@ -19,6 +22,21 @@ const Movie = () => {
 
       <MovieInfo movie={movie} />
       <MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue} />
+      <Grid header='Actors'>
+        {movie.actors.map(actor => (
+          <Actor key={actor.credit_id}
+            name={actor.name}
+            character={actor.character}
+            imageUrl={
+              actor.profile_path
+                ?
+                `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                : NoImage
+            }
+          />
+        ))}
+      </Grid>
+
     </>
   )
 }
